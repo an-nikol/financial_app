e5d84f0a230e7b6b45604037513db5e3be165361
an-nikol <voynskaa@gmail.com>
2023-10-08 16:35:41 +0300

    updated financial_app.py

--- financial_app.py
+++ financial_app.py
@@ -10,7 +10,8 @@ def calculate_compound_interest(principal, rate, time_in_years):
 
 
 def calculate_loan_payment(principal, rate, months):
-    amount = (rate / 12) * (1 / (1 - (1 + (rate / 12) / 100) ** (-months))) * principal
+    converted_rate = rate / 100
+    amount = (converted_rate / 12) * (1 / (1 - (1 + converted_rate / 12) ** (-months))) * principal
     return amount
 
 
@@ -22,6 +23,7 @@ def calculate_future_value_of_savings(present_value, rate, number_of_periods):
 def main():
     while True:
         # create a  menu that displays following options
+        print()
         print('Main Menu:')
         print()
         print('1.Calculate Simple Interest')
@@ -46,21 +48,29 @@ def main():
             interest_rate = int(input('Enter the interest rate: '))
             time_in_years = int(input('Enter the time(in years): '))
             result = calculate_simple_interest(principal, interest_rate, time_in_years)
-            print(f'The simple interest is: {result}')
+            print(f'The simple interest is: {result:.2f}')
         elif operation == 2:
-            pass
+            principal = int(input('Enter the principal amount: '))
+            interest_rate = int(input('Enter the interest rate: '))
+            time_in_years = int(input('Enter the time(in years): '))
+            result = calculate_compound_interest(principal, interest_rate, time_in_years)
+            print(f'The compound interest is: {result:.2f}')
         elif operation == 3:
-            pass
+            principal = int(input('Enter the loan amount: '))
+            interest_rate = int(input('Enter the interest rate: '))
+            time_in_months = int(input('Enter the time(in months): '))
+            result = calculate_loan_payment(principal, interest_rate, time_in_months)
+            print(f'The monthly loan payment is: {result:.2f}')
         elif operation == 4:
-            pass
+            present_value = int(input('Enter the present value: '))
+            interest_rate = int(input('Enter the interest rate: '))
+            number_of_periods = int(input('Enter the number of time periods: '))
+            result = calculate_future_value_of_savings(present_value, interest_rate, number_of_periods)
+            print(f'The future value of your savings is: {result:.2f}')
         elif operation == 5:
-            pass
-        
+            print('Goodbye!')
+            break
 
 
-
-principall = float(input())
-ratee = float(input())
-moths = int(input())
-
-print(calculate_future_value_of_savings(principall, ratee, moths))
+if __name__ == "__main__":
+    main()
